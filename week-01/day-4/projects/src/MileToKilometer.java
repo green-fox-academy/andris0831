3d5ef1160bc535d6523ee35b5a1f7f05502eb39d
andris0831 <andris0831@gmail.com>
2020-11-19 21:09:21 +0100

    Hell yeah

---
+++ week-01/day-4/projects/src/MileToKilometer.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MileToKilometer {
+    public static void main(String[] args) {
+        double changeNumber = 1.6;
+        double userInput1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please give distance in decimal number in miles!");
+
+        userInput1 = scanner.nextDouble();
+        double result = (userInput1 * changeNumber);
+        System.out.println("The distance in kilometers is: " + result);
+
+    }
+}
