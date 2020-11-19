e2938121977e426d33b9aca9ce9a0111ea9a1d40
andris0831 <andris0831@gmail.com>
2020-11-19 16:59:41 +0100

    New files have been added

---
+++ week-01/day-4/projects/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+
+        String name = "Andris";
+        int age = 25;
+        double height = 1.84;
+        boolean marriage = false;
+
+        System.out.println("My name is: " + name);
+        System.out.println("My height is: " + height);
+        System.out.println("I'm married= " + marriage);
+    }
+}
