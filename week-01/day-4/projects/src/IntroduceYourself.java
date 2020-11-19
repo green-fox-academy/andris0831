b158712d4cbd64b669fa2afc6927bbdc37fa8945
andris0831 <andris0831@gmail.com>
2020-11-19 14:24:30 +0100

    IntroduceYourself task has been done

---
+++ week-01/day-4/projects/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Andras Skoperda");
+        System.out.println("25");
+        System.out.println("1.84");
+    }
+}
