58ce3f94d7f47b3733d3200d32e860b92752f365
andris0831 <andris0831@gmail.com>
2020-11-19 16:52:33 +0100

    New files have been addedű

--- week-01/day-4/projects/src/CodingHours.java
+++ week-01/day-4/projects/src/CodingHours.java
@@ -2,18 +2,18 @@ public class CodingHours {
     public static void main(String[] args) {
 
         // Average working hours //
-        int a = 6;
+        double a = 6;
         // The length of the semester //
-        int b = 17;
+        double b = 17;
         // Days of the working week //
-        int c = 5;
+        double c = 5;
         // Average work hours of the week //
-        int d = 52;
+        double d = 52;
         // Hours of the week //
-        int f = 168;
+        double f = 168;
 
-        System.out.println("Hours " + "spent " + "with " + "coding: " + ((a * b * c)) );
-        System.out.println("Percentage" + " of" + " coding" + " hours: " + (((a * b * c) / (b * d)) * 100) + "%");
+        System.out.println("Hours spent with coding: " + (int)((a * b * c)) );
+        System.out.println("Percentage of coding hours: " + (int)(((a * b * c) / (b * d)) * 100) + "%");
 
     }
 }
