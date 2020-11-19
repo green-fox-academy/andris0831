e8ddf7f2cf1a8dd3a04234e08b9d4f3a320345b3
andris0831 <andris0831@gmail.com>
2020-11-19 20:37:13 +0100

    Changes have been made

--- week-01/day-4/projects/src/SecondInADay.java
+++ week-01/day-4/projects/src/SecondInADay.java
@@ -5,6 +5,10 @@ public class SecondInADay {
         int currentMinutes = 34;
         int currentSeconds = 42;
 
-        System.out.println("Remaining seconds of the day: " + ((24 * 360) - ((currentHours * 360) + (currentMinutes * 60) + currentSeconds)));
+        int hoursSeconds = 14 * 360;
+        int hoursMinutes = 34 * 60;
+
+
+        System.out.println("Remaining seconds of the day: " + ((24 * 360) - hoursSeconds - hoursMinutes  - currentSeconds));
     }
 }
