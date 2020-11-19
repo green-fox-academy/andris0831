509fdbd9f7c6b2f401e6218d7595bf7ac4f059d5
andris0831 <andris0831@gmail.com>
2020-11-19 16:16:59 +0100

    New file has been added

---
+++ week-01/day-4/projects/src/Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+
+        int a = 123;
+        int b = 526;
+
+        a = 526;
+        b = 123;
+
+        System.out.println("After " + "swap: " + "a:" + a + " " + "b:" + b);
+    }
+}
