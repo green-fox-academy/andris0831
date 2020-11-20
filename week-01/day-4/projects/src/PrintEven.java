7c6455bfc7c29e99c121528d2fa0eeab246dfe5b
andris0831 <andris0831@gmail.com>
2020-11-20 11:50:59 +0100

    Yeah

--- week-01/day-4/projects/src/PrintEven.java
+++ week-01/day-4/projects/src/PrintEven.java
@@ -3,7 +3,7 @@ public class PrintEven {
         // Create a program that prints all the even numbers between 0 and 500
 
         for (int i = 0; i < 501; i++;) {
-            
+
         }
     }
 }
