69fc9e285865d7299e4c0764a7611f2c0c7033cf
andris0831 <andris0831@gmail.com>
2020-11-20 11:03:17 +0100

    New task has done

---
+++ week-01/day-4/projects/src/IWontCheatOntheExams.java
@@ -0,0 +1,8 @@
+public class IWontCheatOntheExams {
+    public static void main(String[] args) {
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        
+    }
+}
