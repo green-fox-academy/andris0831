6c871d263456417a54c0b102c7ea08286162b66c
andris0831 <andris0831@gmail.com>
2020-11-20 11:19:46 +0100

    modified

--- week-01/day-4/projects/src/IWontCheatOntheExams.java
+++ week-01/day-4/projects/src/IWontCheatOntheExams.java
@@ -3,6 +3,8 @@ public class IWontCheatOntheExams {
         // Create a program that writes this line 100 times:
         // "I won't cheat on the exam!"
 
-        
+        for (int i = 0; i < 101; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
     }
 }
