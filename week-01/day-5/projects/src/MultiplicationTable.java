1dc61861d003cb8ab5063d87cb639c359fb2d994
andris0831 <andris0831@gmail.com>
2020-11-20 13:27:16 +0100

    MultiplicationTable has been added

--- week-01/day-5/projects/src/MultiplicationTable.java
+++ week-01/day-5/projects/src/MultiplicationTable.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
-        
+        // Create a program that asks for a number and prints the multiplication table with that number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
+
+        System.out.println("Please give a number!");
+
+        Scanner scanner = new Scanner(System.in);
+        int userInput1 = scanner.nextInt();
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i + "* " + userInput1 + "= " + (i * userInput1));
+        }
     }
 }
