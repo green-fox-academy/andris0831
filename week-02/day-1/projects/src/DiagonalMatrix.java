4f9530109826e0d3946e2449f2194e32356cddf5
andris0831 <andris0831@gmail.com>
2020-11-23 16:15:15 +0100

    New task completed

---
+++ week-02/day-1/projects/src/DiagonalMatrix.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class DiagonalMatrix {
+    // - Create (dynamically*) a two dimensional array
+    //   with the following matrix**. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+    // * length should depend on a variable
+    // ** Relax, a matrix is just like an array
+
+    public static void main(String[] args) {
+
+    }
+}
+
