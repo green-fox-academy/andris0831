3be48dc85e495c970bd40d4c5d87945e27f5cef7
andris0831 <andris0831@gmail.com>
2020-11-23 15:19:29 +0100

    Hell yeah

--- week-02/day-1/projects/src/Sum.java
+++ week-02/day-1/projects/src/Sum.java
@@ -1,9 +1,26 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 public class Sum {
     // Create the usual class wrapper and main method on your own.
 
 // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
 
-    public static void main(String[] args) {
 
+    public static int adding(int par) {
+        int[] numbers = new int[par + 1];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+        }
+        int sum = 0;
+        for (int j = 0; j < numbers.length; j++) {
+            sum += numbers[j];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(adding(5));
     }
 }
