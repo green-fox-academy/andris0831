182b8bd3e7d3a018ac71beca3534261181d8f25e
andris0831 <andris0831@gmail.com>
2020-11-24 16:04:46 +0100

    ArrayList introduction

---
+++ week-02/day-2/projects/src/ListIntroduction1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class ListIntroduction1 {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("William");
+        names.add("John");
+        names.add("Amanda");
+
+        System.out.println(names.toString());
+        System.out.println(names.get(2));
+
+        for (String i : names) {
+            System.out.println(i);
+
+        }for (int i = 0; i < names.size(); i++) {
+            System.out.println(i + 1 + ". " + names.get(i));
+        }
+
+        names.remove(1);
+
+        for (int i = names.size() - 1; i >= 0; i--) {
+            System.out.println(names.get(i));
+        }
+    }
+}
