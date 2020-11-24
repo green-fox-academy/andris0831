8ff244b44eeb8eac05fa8037a14e7fa84034e95b
andris0831 <andris0831@gmail.com>
2020-11-24 13:19:46 +0100

    New tasks

---
+++ week-02/day-2/projects/src/SimpleReplace.java
@@ -0,0 +1,12 @@
+public class SimpleReplace {
+    // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+    // Please fix it for me!
+    // Expected output: In a galaxy far far away
+
+    public static void main(String[] args) {
+        String example = "In a dishwasher far far away";
+
+        String newExample = example.replace("dishwasher", "galaxy");
+        System.out.println(newExample);
+    }
+}
