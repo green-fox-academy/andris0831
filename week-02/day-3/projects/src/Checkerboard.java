b063355929436f556676d83b40cf4df6ca91baa7
andris0831 <andris0831@gmail.com>
2020-11-26 20:11:48 +0100

    Chess

--- week-02/day-3/projects/src/Checkerboard.java
+++ week-02/day-3/projects/src/Checkerboard.java
@@ -5,8 +5,34 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Checkerboard {
+        public static void checkers(Graphics graphics) {
+            int x = 0;
+            int y = 0;
+            int size = 10;
+            boolean blue = true;
+            int width = 40;
+            int height = 40;
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    x += 40;
+                    if (blue == true) {
+                        graphics.setColor(Color.BLUE);
+                    } else {
+                        graphics.setColor(Color.BLACK);
+                    }
+                    graphics.fillRect(x, y, width, height);
+                    blue = !blue;
+                }
+                blue = !blue;
+                x = 0;
+                y += 40;
+            }
+        }
     public static void mainDraw(Graphics graphics) {
         // Fill the canvas with a checkerboard pattern.
+
+        checkers(graphics);
 
     }
 
