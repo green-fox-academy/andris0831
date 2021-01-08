29442b94fceb11cf7496f9f675ab10891a362634
andris0831 <andris0831@gmail.com>
2021-01-08 21:10:31 +0100

    Webshop

--- week-06/day-5/src/main/java/com/greenfoxacademy/workshop/controller/WebshopController.java
+++ week-06/day-5/src/main/java/com/greenfoxacademy/workshop/controller/WebshopController.java
@@ -15,7 +15,7 @@ public class WebshopController {
      private List<ShopItem> shopItems = new ArrayList<>();
 
     public WebshopController(){
-        shopItems.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000D, 5));
+        //shopItems.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000D, 5));
         shopItems.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000D, 2));
         shopItems.add(new ShopItem("Coca cola", "0.5l standard cola", 25D, 0));
         shopItems.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119D, 100));
